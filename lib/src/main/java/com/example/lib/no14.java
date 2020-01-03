adad5dc254bb41f03aaf81ff671919bb13e1b14f
cyk90290 <cyk87321@yahoo.com.tw>
2020-01-03 15:21:04 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/no14.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+import java.util.Scanner;
+public class no14 {
+    public static void main(String[] args) {
+        String str = "Taiwan, Touch, Your, Heart";
+        String strChange = "";
+
+        int strlength = str.length();
+        for (int i = 0; i < strlength; i++) {
+            if (str.charAt(i)!=(char)44) {
+                strChange += str.charAt(i);
+            }
+        }
+        System.out.println(strChange);
+    }
+}
